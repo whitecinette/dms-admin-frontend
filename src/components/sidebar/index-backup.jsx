15eee556e7d9c55a8dd35d4034a0307a867c1be2
Rakshita Kanwar <rakshitakanwar09@gmail.com>
2025-08-18 17:48:12 +0530

    side

--- src/components/sidebar/index-backup.jsx
+++ src/components/sidebar/index-backup.jsx
@@ -23,6 +23,10 @@ import logo from "../../assets/images/company-logo.png";
 import "./style.scss";
 import { FcGlobe } from "react-icons/fc";
 
+ // import axios from "axios";
+ // import config from "../../config";
+ //
+ // const backend_url = config.backend_url;
 
 function Sidebar({ isCollapsed, open, toggleSidebar }) {
   const navigate = useNavigate();
@@ -37,6 +41,24 @@ function Sidebar({ isCollapsed, open, toggleSidebar }) {
   };
   const [isMobile, setIsMobile] = useState(window.innerWidth < 1024); // Track screen size
 
+   // handle logout
+   // const handleLogout = async () => {
+   //   try {
+   //     await axios.post(
+   //       `${backend_url}/user/logout`,
+   //       {}, // empty body
+   //       {
+   //         headers: {
+   //           Authorization: localStorage.getItem("authToken"),
+   //         },
+   //       }
+   //     );
+   //     localStorage.clear();
+   //     navigate("/login");
+   //   } catch (error) {
+   //     console.error("Error during logout:", error);
+   //   }
+   // };
 
   const navItems = [
     {
@@ -101,6 +123,7 @@ function Sidebar({ isCollapsed, open, toggleSidebar }) {
       onClick: () => open(),
       children: [
         { name: "Attendance", to: "/attendance" },
+        { name: "Attendance Matrixx", to: "/attendance-matrix" },
         { name: "Leave Application", to: "/leaveApplication" },
         { name: "Travel Expenses", to: "/travelExpenses" },
         { name: "Payroll", to: "/payroll" },
@@ -110,6 +133,21 @@ function Sidebar({ isCollapsed, open, toggleSidebar }) {
         { name: "Voucher", to: "/salesDate" },
       ],
     },
+    // { name: "HR", to: "/hrDashboard", icon: <svg width="20" height="20" viewBox="0 0 60 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+    //     <path d="M58.7419 33.871H1.25806C0.580645 33.871 0 34.4516 0 35.129V39.8715C0 40.5484 0.580645 41.1295 1.25806 41.1295H58.7419C59.4189 41.1295 60 40.5489 60 39.8715V35.129C60 34.4516 59.4194 33.871 58.7419 33.871Z" fill="currentColor"/>
+    //     <path d="M1.30645 28.0645H7.06452C7.8871 28.0645 8.70968 27.2419 8.70968 26.4194V18C8.70968 17.1774 9.19355 16.4516 10.0161 16.4516H16.5968C17.4194 16.4516 17.9516 17.1774 17.9516 18V26.4677C17.9516 27.2903 18.7258 28.1124 19.5484 28.1124H25.3548C26.1774 28.1124 26.6613 27.2903 26.6613 26.4677V1.20968C26.6613 0.387097 26.1774 0 25.3548 0H19.5484C18.7258 0 17.9516 0.435484 17.9516 1.20968V8.12903C17.9516 8.95161 17.4194 9.67742 16.5968 9.67742H10.0161C9.19355 9.67742 8.70968 8.95161 8.70968 8.12903V1.20968C8.70968 0.387097 7.8871 0 7.06452 0H1.30645C0.483871 0 0 0.435484 0 1.20968V26.4194C0 27.2419 0.483871 28.0645 1.30645 28.0645Z" fill="currentColor"/>
+    //     <path d="M53.177 14.9032C53.612 14.7581 53.9512 14.5645 54.2899 14.371C55.4507 13.6452 56.3706 12.7258 57.048 11.6129C57.7249 10.5 58.0641 9.14516 58.0641 7.59677C58.0641 5.80645 57.6286 4.30645 56.7577 3.04839C55.8867 1.79032 54.7254 1.06452 53.3222 0.580646C51.9189 0.0967746 49.8383 0 47.177 0H33.4351C33.0001 0 32.4189 0.0967744 32.4189 0.532258V27.1452C32.4189 27.5806 32.9996 28.0645 33.4351 28.0645H40.6448C41.0802 28.0645 41.6609 27.5806 41.6609 27.1452V17.2258C41.6609 16.7903 41.9028 16.4516 42.2904 16.4516C43.0646 16.4516 43.742 16.6452 44.371 17.0806C44.8549 17.4194 45.3388 18.1452 45.9194 19.2581L50.3227 27.5806C50.4678 27.8226 50.7098 28.0645 51.0001 28.0645H59.1291C59.7097 28.0645 60.0968 27.3871 59.806 26.8548C58.548 24.3871 57.4356 21.7742 55.8388 19.4516C55.2093 18.5323 54.5323 17.4677 53.7098 16.7419C53.177 16.3064 52.1125 15.3387 53.177 14.9032ZM48.8706 9.72581C48.4351 10.3065 47.7577 10.6935 47.0318 10.8871C45.4835 11.3226 43.8378 11.1774 42.2415 11.1774C41.806 11.1774 41.6125 10.8387 41.6125 10.4032V6.19355C41.6125 5.75806 41.7093 5.32258 42.1448 5.32258H45.1931C46.548 5.32258 47.9996 5.41935 48.8706 6.67742C49.4996 7.54839 49.4996 8.85484 48.8706 9.72581Z" fill="currentColor"/>
+    //     </svg>,
+    //     onClick: ()=> open(),
+    //     children:  [
+    //         {name:"Attendance", to:"/attendance",},
+    //         {name:"Approval", to:"/salesDate",},
+    //         {name:"Payroll", to:"/salesDate",},
+    //         {name:"View Beat Mapping Status", to:"/viewBeatMappingStatus",},
+    //         {name:"Employee Management", to:"/salesDate",},
+    //         {name:"Voucher", to:"/salesDate",},
+    //     ]
+    //      },
     {
       name: "Users",
       to: "#",
@@ -146,7 +184,11 @@ function Sidebar({ isCollapsed, open, toggleSidebar }) {
         { name: "View Outstanding Data", to: "/finance/vouchers" },
       ],
     },
+    // { name: "Beat Mapping", to: "/segment", icon: <LuMapPinHouse size={20} /> },
+    // { name: "Format", to: "/segment", icon: <FaFileAlt size={20} /> },
+    // { name: "Upload", to: "/segment", icon: <BsCloudUpload size={20} /> },
     { name: "Target", to: "/segment", icon: <TbTargetArrow size={20} /> },
+    // { name: "Hierarchy", to: "/segment", icon: <TbHierarchy3 size={20} /> },
     {
       name: "Employee Time Line",
       to: "/segment",
